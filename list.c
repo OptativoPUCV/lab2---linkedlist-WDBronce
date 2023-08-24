c2f2bc650107e5525f24168a66ffc5a7af24152d
Replit user <>
2023-08-24 19:17:30 +0000

    new try

--- list.c
+++ list.c
@@ -108,18 +108,17 @@ void pushFront(List * list, void * data)
 /*Programe la función `void pushFront(List * list, void * data)`, la cual agrega un dato al comienzo de la lista. 
 > Puede utilizar la función `Node* createNode(void * data)` la cual crea, incializa y retorna un nodo con el dato correspondiente. */
 {
-  if (list== NULL || list->current == NULL)
+  if (list== NULL || list->current == NULL || list->head == NULL)
   {
     return;
   }
-  Node* NuevoNodo = createNode(data);
-  if (list->head == NULL)
+  else
   {
+    Node* NuevoNodo = createNode(data);
+    NuevoNodo->next = list->head;
+    NuevoNodo->prev = NULL;
     list->head = NuevoNodo;
   }
-  NuevoNodo->next = list->head;
-  NuevoNodo->prev = NULL;
-  list->head = NuevoNodo;
 }
 
 void pushBack(List * list, void * data) 
